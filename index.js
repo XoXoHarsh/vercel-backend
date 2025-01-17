2f7689b4f7f286c384e02317b6fb4e5409568b91
Harsh Sharma <harshsharma20503@gmail.com>
2025-01-17 10:26:49 +0530

    added cors

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
 
 app.use(express.json());
 
